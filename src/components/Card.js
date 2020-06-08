cba848f0186419ddc3d429353e1c196a0e28e156
Oteri Eyenike <teyenike1@gmail.com>
2020-06-08 03:00:39 +0100

    added the manifest and favicon for the devices

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({ id, email, name }) => {
   return (
     <div className='pointer tc bg-light-blue dib br4 pa3 ma2 grow bw2 shadow-5'>
-      <img src={`https://robohash.org/${id}?200x200`} alt='robots' className="w-100" />
+      <img src={`https://robohash.org/${id}?size=200x200`} alt='robots' className="w-100" />
       <div className='details'>
         <h2>{name}</h2>
         <p>{email}</p>
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,6 +4,7 @@ const SearchBox = ({ searchfield, searchChange }) => {
   return (
     <div className='pa2'>
       <input
+        aria-label='Search Robots'
         className='pa3 ba b--blue bg-light-blue'
         type='search'
         placeholder='Search robots'
