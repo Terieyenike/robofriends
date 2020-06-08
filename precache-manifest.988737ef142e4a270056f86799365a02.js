f90a8c563721d4567bc32b1e0982a74d739d67cf
Oteri Eyenike <teyenike1@gmail.com>
2020-06-08 02:34:30 +0100

    Updates

---
+++ precache-manifest.988737ef142e4a270056f86799365a02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "252e0f3d98235e8d5dcea0d7552bebdf",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "627f8b96b3e5c2286d28",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "12c69f12c956cb87f46e",
+    "url": "/robofriends/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "627f8b96b3e5c2286d28",
+    "url": "/robofriends/static/js/2.6c9d5929.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/robofriends/static/js/2.6c9d5929.chunk.js.LICENSE"
+  },
+  {
+    "revision": "12c69f12c956cb87f46e",
+    "url": "/robofriends/static/js/main.9af245f7.chunk.js"
+  },
+  {
+    "revision": "1e6fdd492adfb4acd267",
+    "url": "/robofriends/static/js/runtime-main.2c4b189a.js"
+  }
+]);
